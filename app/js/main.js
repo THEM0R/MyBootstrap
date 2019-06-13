48e24490f1a62a4989949bc254459fcf072bdbaa
Yaroslav <the.mor.ua@gmail.com>
2019-06-13 21:58:23 +0300

    0.4

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
+/* requires */
+//=require libs/jquery.min.js
+// bootstrap
+//=require libs/popper.min.js
+//=require ../bootstrap-4.3.1/js/util.js
+//=require ../bootstrap-4.3.1/js/collapse.js
+
 $(function () {
 
 });
