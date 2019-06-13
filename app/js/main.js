bf0542ec713fa21f686167779b069dc69cf642dc
Yaroslav <the.mor.ua@gmail.com>
2019-06-13 23:54:07 +0300

    0.6.2 sticky-top

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,30 @@
 //=require ../bootstrap-4.3.1/js/util.js
 //=require ../bootstrap-4.3.1/js/collapse.js
 
+// justifiedGallery
+//=require libs/justifiedGallery/jquery.justifiedGallery.min.js
+
+/* requires */
 $(function () {
+
+  $("#mygallery").justifiedGallery({
+
+    rowHeight : 150,
+    lastRow : 'nojustify',
+    margins : 3
+
+  });
+
+
+  function getRandomSize(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+
+  for (var i = 0; i < 25; i++) {
+    var width = getRandomSize(200, 400);
+    var height = getRandomSize(200, 400);
+
+    $('#photos1').append('<img src="//www.lorempixel.com/' + width + '/' + height + '/cats" alt="pretty kitty">');
+  }
 
 });
